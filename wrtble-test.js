024cdbc9d1c7061e45979e3c88eb20b7d801c985
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-13 16:09:51 +0800

    Updated server/client test.

--- wrtble-test.js
+++ wrtble-test.js
@@ -1,7 +1,12 @@
 
 const assert = require( "assert" );
+const harden = require( "harden" );
 const wrtble = require( "./wrtble.js" );
 
 assert.equal( wrtble( "property", { "property": "value" } ), true, "should be true" );
+
+let test = { };
+harden( 123, "sample", test );
+assert.equal( wrtble( 123, test ), false, "should be false" );
 
 console.log( "ok" );
