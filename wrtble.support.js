0e9fc6c8d2a764cd2e69dda196ef4b708ff36a3c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-26 11:04:16 +0800

    Updated dependency.

--- wrtble.support.js
+++ wrtble.support.js
@@ -32,10 +32,11 @@
               			"file": "wrtble.js",
               			"module": "wrtble",
               			"author": "Richeve S. Bebedor",
-              			"contributors": [
-              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-              			],
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/wrtble.git",
               			"test": "wrtble-test.js",
               			"global": true
